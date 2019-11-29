dffe57a125c27197454b6dd977b82b80b2e7320c
Shantanu Singh <shantanu.singh@tesco.com>
2019-11-30 01:17:04 +0530

    Initial commit

---
+++ data-preprocess.py
@@ -0,0 +1,84 @@
+import numpy as np
+import os
+import scipy.io as sio
+import h5py
+# import DigitStructWrapper
+
+
+DATA_DIR = "data"
+TRAIN_DIR = os.path.join(DATA_DIR, 'train')
+TEST_DIR = os.path.join(DATA_DIR, 'test')
+EXTRA_DIR = os.path.join(DATA_DIR, 'extra')
+
+
+def load_data(path):
+
+    data = sio.loadmat(path+'_32x32.mat')
+    return data
+
+
+def normalize_images(images):
+
+    imgs = images["X"]
+    imgs = np.transpose(imgs, (3, 0, 1, 2))
+
+    labels = images["y"]
+    # replace label "10" with label "0"
+    labels[labels == 10] = 0
+
+    # normalize images so pixel values are in range [0,1]
+    scalar = 1 / 255.
+    imgs = imgs * scalar
+
+    return imgs, labels
+
+
+def save_data(images, labels, name):
+    with h5py.File(name+".hdf5", "w") as f:
+        f.create_dataset("X", data=images, shape=images.shape, dtype='float32', compression="gzip")
+        f.create_dataset("Y", data=labels, shape=labels.shape, dtype='int32', compression="gzip")
+
+
+def get_box_data(index, hdf5_data):
+
+    meta_data = dict()
+    meta_data['height'] = []
+    meta_data['label'] = []
+    meta_data['left'] = []
+    meta_data['top'] = []
+    meta_data['width'] = []
+
+    def print_attrs(name, obj):
+        vals = []
+        if obj.shape[0] == 1:
+            vals.append(obj[0][0])
+        else:
+            for k in range(obj.shape[0]):
+                vals.append(int(hdf5_data[obj[k][0]][0][0]))
+        meta_data[name] = vals
+
+    box = hdf5_data['/digitStruct/bbox'][index]
+    hdf5_data[box[0]].visititems(print_attrs)
+    return meta_data
+
+
+def get_name(index, hdf5_data):
+    name = hdf5_data['/digitStruct/name']
+    return ''.join([chr(v[0]) for v in hdf5_data[name[index][0]].value])
+
+
+
+if __name__ == "__main__":
+
+    # train_data = load_data(TRAIN_DIR)
+    # test_data = load_data(TEST_DIR)
+    extra_data = load_data(EXTRA_DIR)
+
+    # train_images_normalized, train_labels = normalize_images(train_data)
+    # save_data(train_images_normalized, train_labels, "SVHN_train")
+    #
+    # test_images_normalized, test_labels = normalize_images(test_data)
+    # save_data(test_images_normalized, test_labels, "SVHN_test")
+
+    extra_images_normalized, extra_labels = normalize_images(extra_data)
+    save_data(extra_images_normalized, extra_labels, "SVHN_extra")
---
+++ train_classifier.py
@@ -0,0 +1,72 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+# import matplotlib.pyplot as plt
+
+
+def custom_cnn():
+    # load data
+
+    # construct model
+    model = models.Sequential()
+
+    model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Conv2D(128, (5, 5), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Conv2D(256, (5, 5), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dense(num_classes, activation='softmax'))
+
+    # model.add(Conv2D(filters=32, kernel_size=5, strides=5, border_mode='same',
+    #                  input_shape=input_shape))
+    # model.add(Activation('relu'))
+    # model.add(Conv2D(32, 5, 5, border_mode='same'))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.20))
+    #
+    # model.add(Conv2D(64, 3, 3, border_mode='same'))
+    # model.add(Activation('relu'))
+    # model.add(Conv2D(64, 3, 3, border_mode='same'))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.20))
+    #
+    # model.add(Conv2D(64, 1, 1, border_mode='same'))
+    # model.add(Activation('relu'))
+    # model.add(Conv2D(64, 1, 1, border_mode='same'))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.20))
+    #
+    # model.add(Flatten())
+    # model.add(Dense(512))
+    # model.add(Activation('relu'))
+    # model.add(Dropout(0.25))
+    # model.add(Dense(num_classes))
+    # model.add(Activation('softmax'))
+
+    #  Stochastic Gradient Descent
+    # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+
+    # model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    history = model.fit(train_images, train_labels, epochs=10,
+                        validation_data=(test_images, test_labels))
