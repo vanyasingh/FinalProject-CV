3efd537fa24a20dda0ffea3dd9ecb324dccb37e0
Shantanu Singh <shantanu.singh@tesco.com>
2019-11-30 19:24:40 +0530

    first commit

--- train_classifier.py
+++ train_classifier.py
@@ -1,14 +1,23 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
+import os
+from tensorflow.keras import layers, models
+import data_preprocess
 
-from tensorflow.keras import datasets, layers, models
 # import matplotlib.pyplot as plt
+
+DATA_DIR = "data"
+TRAIN_DIR = os.path.join(DATA_DIR, 'train')
+TEST_DIR = os.path.join(DATA_DIR, 'test')
+EXTRA_DIR = os.path.join(DATA_DIR, 'extra')
 
 
 def custom_cnn():
-    # load data
-
+    # load train data
+    train_images, train_labels, train_bbox = data_preprocess.get_processed_data(TRAIN_DIR)
+    # load test data
+    test_images, test_labels, test_bbox = data_preprocess.get_processed_data(TEST_DIR)
     # construct model
     model = models.Sequential()
 
@@ -29,44 +38,10 @@ def custom_cnn():
 
     model.add(layers.Flatten())
     model.add(layers.Dense(512, activation='relu'))
-    model.add(layers.Dense(num_classes, activation='softmax'))
+    model.add(layers.Dense(10, activation='softmax'))
 
-    # model.add(Conv2D(filters=32, kernel_size=5, strides=5, border_mode='same',
-    #                  input_shape=input_shape))
-    # model.add(Activation('relu'))
-    # model.add(Conv2D(32, 5, 5, border_mode='same'))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
-    # model.add(Dropout(0.20))
-    #
-    # model.add(Conv2D(64, 3, 3, border_mode='same'))
-    # model.add(Activation('relu'))
-    # model.add(Conv2D(64, 3, 3, border_mode='same'))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
-    # model.add(Dropout(0.20))
-    #
-    # model.add(Conv2D(64, 1, 1, border_mode='same'))
-    # model.add(Activation('relu'))
-    # model.add(Conv2D(64, 1, 1, border_mode='same'))
-    # model.add(Activation('relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
-    # model.add(Dropout(0.20))
-    #
-    # model.add(Flatten())
-    # model.add(Dense(512))
-    # model.add(Activation('relu'))
-    # model.add(Dropout(0.25))
-    # model.add(Dense(num_classes))
-    # model.add(Activation('softmax'))
-
-    #  Stochastic Gradient Descent
-    # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-
-    # model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
     model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
-    history = model.fit(train_images, train_labels, epochs=10,
-                        validation_data=(test_images, test_labels))
+    history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
