6fd95813c27766ce5efa83c665e8189c71ddfb1a
Shantanu Singh <shantanu.singh@tesco.com>
2019-12-02 10:26:48 +0530

    vanya 2

--- data_preprocess.py
+++ data_preprocess.py
@@ -15,24 +15,15 @@ EXTRA_DIR = os.path.join(DATA_DIR, 'extra')
 
 def load_data(path):
 
-    data = sio.loadmat(path+'_32x32.mat')
-    return data
+    # data = sio.loadmat(path+'_32x32.mat')
+    # mat_data = sio.loadmat(os.path.join(path, 'digitStruct.mat'))
+    mat_data = h5py.File(os.path.join(path, 'digitStruct.mat'))
+    data = mat_data["digitStruct"]
 
+    bbox = data['bbox']
+    name = data['name']
 
-def normalize_images(images):
-
-    imgs = images["X"]
-    imgs = np.transpose(imgs, (3, 0, 1, 2))
-
-    labels = images["y"]
-    # replace label "10" with label "0"
-    labels[labels == 10] = 0
-
-    # normalize images so pixel values are in range [0,1]
-    scalar = 1 / 255.
-    imgs = imgs * scalar
-
-    return imgs, labels
+    return name, bbox
 
 
 def get_box_data(index, hdf5_data):
@@ -63,7 +54,7 @@ def get_image(index, hdf5_data):
     return ''.join([chr(v[0]) for v in hdf5_data[name[index][0]].value])
 
 
-def get_processed_data(path):
+def get_train_data(path):
 
     print("RUNNING PROCESS DATA")
 
@@ -72,44 +63,76 @@ def get_processed_data(path):
 
     train_images = []
     train_labels = []
-    train_bbox = []
 
-    for _i in tqdm.tqdm(range(size)):
-        image_name = get_image(_i, mat_data)
-        train_images.append(cv2.imread(os.path.join(TRAIN_DIR, image_name)))
+    dim = (64, 64)
 
-        box = get_box_data(_i, mat_data)
-        train_bbox.append(box)
+    for i in tqdm.tqdm(range(10)):
+        image_name = get_image(i, mat_data)
+        image = cv2.imread(os.path.join(TRAIN_DIR, image_name))
 
-        labels = box['label']
-        train_labels.append(labels)
+        box = get_box_data(i, mat_data)
 
-    train_images = np.asarray(train_images)
-    train_labels = np.asarray(train_labels)
-    train_bbox = np.asarray(train_bbox)
+        x = int(min(box['top']))
+        y = int(min(box['left']))
+        h = int(max(box['height'])) + x
+        w = int(sum(box['width'])) + y
 
-    train_images.astype('float32')
-    train_labels.astype('int32')
-    train_bbox.astype('int32')
+        # crop and resize image
+        crop_im = image[x:h, y:w, :]
+        fin_im = cv2.resize(crop_im, dim, interpolation=cv2.INTER_AREA)
+        fin_im = np.float32(fin_im) / 255.
 
-    train_images /= 255
-    np.asarray(train_bbox)
-    np.asarray(train_labels)
+        train_images.append(fin_im)
+
+        # img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # mser = cv2.MSER()
+        # regions = mser.detect(img, None)
+        # print(regions)
+
+        print(box['label'])
+        pad = np.zeros(6)
+
+
+
+    #
+    #     labels = box['label'].squeeze()
+    #     train_labels.append(np.int32(labels))
+    #
+    #
+    # train_images = np.array(train_images)
+    #
+    # # print(train_images.shape)
+    # train_labels = np.array(train_labels)
+    # print(train_labels)
+    # print(train_labels.shape)
+    # train_bbox = np.array(train_bbox)
+
+    # train_images = np.asarray(train_images)
+    # train_images.astype('float32')
+
+    # train_images /= 255
 
     return train_images, train_labels, train_bbox
 
 
 def save_data(images, labels, bbox, name):
     print("SAVING DATA")
-    with h5py.File(name+".hdf5", "w") as f:
-        f.create_dataset("X", data=images, shape=images.shape, dtype='float32', compression="gzip")
-        f.create_dataset("Y", data=labels, shape=labels.shape, dtype='int32', compression="gzip")
-        f.create_dataset("bbox", data=bbox, shape=bbox.shape, dtype='int32', compression="gzip")
+    hf = h5py.File(name+".h5", 'w')
+    hf.create_dataset('X', data=images)
+    hf.create_dataset('Y', data=labels)
+
+    # with h5py.File(name+".hdf5", "w") as f:
+    #     f.create_dataset("X", data=images, shape=images.shape,dtype='float32', compression="gzip")
+    #     f.create_dataset("Y", data=labels, shape=labels.shape, dtype='int32', compression="gzip")
+    #     # f.create_dataset("bbox", data=bbox, shape=bbox.shape, dtype='int32', compression="gzip")
 
 
 if __name__ == "__main__":
+    # image = cv2.imread(os.path.join(TRAIN_DIR, '1.png'))
+    # cv2.imshow('image', image)
+    # cv2.waitKey(0)
 
-    train_images, train_labels, train_bbox = get_processed_data(TRAIN_DIR)
+    train_images, train_labels, train_bbox = get_train_data(TRAIN_DIR)
     save_data(train_images, train_labels, train_bbox, "SVHN_train")
 
 #
